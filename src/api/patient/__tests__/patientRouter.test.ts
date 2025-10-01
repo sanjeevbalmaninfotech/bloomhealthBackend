7f24588555a67579a8273360a77a6a91b26a8a1d
Sanjeev <sanjeevbalmaninfotech@gmail.com>
2025-10-01 17:30:49 +0530

    FIX-  register with main node backend

--- src/api/patient/__tests__/patientRouter.test.ts
+++ src/api/patient/__tests__/patientRouter.test.ts
@@ -10,32 +10,6 @@ describe("Patient API Endpoints", () => {
     it("should return a list of patients", async () => {});
   });
 
-  describe("GET /patients/:id", () => {
-    it("should return a not found error for non-existent ID", async () => {
-      // Arrange
-      const testId = Number.MAX_SAFE_INTEGER;
-
-      // Act
-      const response = await request(app).get(`/patients/${testId}`);
-      const responseBody: myResponse = response.body;
-
-      // Assert
-      expect(response.statusCode).toEqual(StatusCodes.NOT_FOUND);
-      expect(responseBody.success).toBeFalsy();
-      expect(responseBody.responseObject).toBeNull();
-    });
-
-    it("should return a bad request for invalid ID format", async () => {
-      const invalidInput = "abc";
-      const response = await request(app).get(`/patients/${invalidInput}`);
-      const responseBody: myResponse = response.body;
-
-      //  expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-      expect(responseBody.success).toBeFalsy();
-      expect(responseBody.responseObject).toBeNull();
-    });
-  });
-
   describe("POST /patients/register", () => {
     it("should accept a valid registration payload", async () => {
       const payload = {
